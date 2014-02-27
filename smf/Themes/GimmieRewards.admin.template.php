8c56f50b307b50100fcf96a8ccc32ab65050692a
Maythee Anegboonlap <null@llun.in.th>
2014-02-27 18:17:58 +0800

    Maked popup show.

--- smf/Themes/GimmieRewards.admin.template.php
+++ smf/Themes/GimmieRewards.admin.template.php
@@ -6,9 +6,12 @@ if (!defined('SMF')) {
 function template_gimmie_rewards_config() {
 
   global $txt, $context, $scripturl, $modSettings, $settings, $actionArray;
+  
+  $countries = json_decode(file_get_contents('https://raw.github.com/mledoze/countries/master/dist/countries.json'));
 ?>
 
   <div class="admincenter">
+  
     <form method="post" action="<?php echo $scripturl; ?>?action=gmss">
       <div class="cat_bar">
         <h3 class="catbg">Gimmie</h3>
@@ -16,7 +19,7 @@ function template_gimmie_rewards_config() {
 
       <div class="windowbg2">
         <span class="topslice"></span>
-        <div class="content">
+        <div class="content gimmie">
           <dl class="settings">
             <dt>
               <span><label for="gimmieEnable">Enable Gimmie</label></span>
@@ -32,16 +35,52 @@ function template_gimmie_rewards_config() {
               <span><label for="gimmieKey">Key</label></span>
             </dt>
             <dd>
-              <input type="text" name="gm_settings[gm_key]" id="gimmieKey" size="50" class="input_text" value="<?php echo (isset ($modSettings['gm_key']) ? htmlspecialchars($modSettings['gm_key']) : ''); ?>">
+              <input type="text" name="gm_settings[gm_key]" id="gimmieKey" size="50" class="input_text gm-input" value="<?php echo (isset ($modSettings['gm_key']) ? htmlspecialchars($modSettings['gm_key']) : ''); ?>">
             </dd>
             <dt>
               <span><label for="gimmieSecret">Secret</label></span>
             </dt>
             <dd>
-              <input type="text" name="gm_settings[gm_secret]" id="gimmieSecret" size="50" class="input_text" value="<?php echo (isset($modSettings['gm_secret']) ? htmlspecialchars($modSettings['gm_secret']) : ''); ?>">
+              <input type="text" name="gm_settings[gm_secret]" id="gimmieSecret" size="50" class="input_text gm-input" value="<?php echo (isset($modSettings['gm_secret']) ? htmlspecialchars($modSettings['gm_secret']) : ''); ?>">
+            </dd>
+            
+            <dt>
+              <span><label for="gimmieCountry">Rewards Country</label></span>
+            </dt>
+            <dd>
+              <select id="gimmieCountry" name="gm_settings[gm_country]" class="gimmie-config-select gm-select">
+                <option value="auto">Auto Select</option>
+                <?php
+                if ($countries) {
+                  foreach($countries as &$country) {
+                    ?>
+                    <option value="<?php echo $country->{'cca2'}; ?>"><?php echo $country->{'name'}; ?></option>
+                    <?php
+                  }
+                }
+                ?>
+              </select>
             </dd>
           </dl>
 
+          <hr class="hrcolor clear">
+          
+          <dl class="settings">
+            <dd>
+              <dt><span><label for="gimmiePopup">Popup Views</label></span></dt>
+            </dd>
+            <dd>
+              <input id="gimmieViewCatalog" type="checkbox" name="gm_settings[gm_views][catalog]" value="1" checked disabled="" />
+              <label for="gimmieViewCatalog">Catalog</label>
+              
+              <input id="gimmieViewProfile" type="checkbox" name="gm_settings[gm_views][profile]" value="1" />
+              <label for="gimmieViewProfile">Profile</label>
+              
+              <input id="gimmieViewLeaderboard" type="checkbox" name="gm_settings[gm_views][leaderboard]" value="1" />
+              <label for="gimmieViewLeaderboard">Leaderboard</label>
+            </dd>
+          </dl>
+          
           <hr class="hrcolor clear">
           
           <div class="righttext">
