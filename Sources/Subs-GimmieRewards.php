4c048b7528cb37d51e6a767aa76dc8952fad8ec8
Maythee Anegboonlap <null@llun.in.th>
2014-05-30 15:25:31 +0800

    Changed event name to match new convention.

--- Sources/Subs-GimmieRewards.php
+++ Sources/Subs-GimmieRewards.php
@@ -1,19 +1,19 @@
 <?php
-/* 
+/*
  * The MIT License (MIT)
- * 
+ *
  * Copyright (c) 2014 Gimmieworld pte ltd.
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -31,11 +31,11 @@ if (!defined('SMF')) {
  */
 function gimmie_menu_buttons_hook(&$menu_buttons) {
   global $txt, $modSettings;
-  
+
   if (!is_gimmie_enabled()) {
     return;
   }
-  
+
   loadLanguage('GimmieRewards');
   $menu_buttons = array_merge(
     array_slice($menu_buttons, 0, 1, true),
@@ -53,7 +53,7 @@ function gimmie_menu_buttons_hook(&$menu_buttons) {
 
 function gimmie_admin_area_hook(&$admin_area) {
   global $txt, $scopeurl;
-  
+
   loadLanguage('GimmieRewards');
   $admin_area = array_merge(
     $admin_area,
@@ -93,55 +93,55 @@ function gimmie_actions_hook(&$actions) {
 
 function gimmie_load_theme_hook() {
   global $context, $settings, $modSettings, $scripturl, $user_info;
-    
+
   $themeurl = $settings['theme_url'];
   $user = $context['user'];
-  
-  
+
+
   $headers = $context['html_headers'];
   $headers = $headers.<<<EOH
-  
+
   <link href="$themeurl/css/GimmieRewards.css" rel="stylesheet" />
-  
+
 EOH;
 
   if (!empty($modSettings['gm_enable']) && $modSettings['gm_enable']) {
     $endpoint = $scripturl."?action=gmpx;sa=";
     $key = $modSettings['gm_key'];
     $notification_timeout = isset($modSettings['gm_notification_timeout']) ? $modSettings['gm_notification_timeout'] : 10;
-  
+
     $hide_profile_page = (isset($modSettings['gm_views_profile']) && ($modSettings['gm_views_profile'] == 0)) ? 'true' : 'false';
     $hide_leaderboard_page = (isset($modSettings['gm_views_leaderboard']) && ($modSettings['gm_views_leaderboard'] == 0)) ? 'true' : 'false';
-    
+
     $country = '';
-    
+
     if (!isset($modSettings['gm_country']) || $modSettings['gm_country'] == 'auto') {
       $ip = isset($user_info['ip2']) ? $user_info['ip2'] : $user_info['ip'];
       $country = trim(file_get_contents('http://api.wipmania.com/'.$ip));
     }
     else {
       $country = $modSettings['gm_country'];
-    }    
-  
+    }
+
     $localize_text = isset($modSettings['gm_localize']) ? $modSettings['gm_localize'] : 'us';
     $help_text = isset($modSettings['gm_help_text']) ? $modSettings['gm_help_text'] : '';
     $help_url = isset($modSettings['gm_help_url']) ? $modSettings['gm_help_url'] : '';
-  
+
     $headers = $headers.<<<EOS
   <script type="text/javascript">
     _gimmie = {
       "endpoint"                    : "$endpoint",
       "key"                         : "$key",
-      "country"                     : "$country",  
+      "country"                     : "$country",
 EOS;
-    
+
     if (!$user['is_guest']) {
       $email = $user['email'];
       $username = $user['username'];
       $name = $user['name'];
-      
+
       $headers = $headers.<<<EOU
-      
+
       "user"                        : {
           "external_uid"              : "$email",
           // Display name
@@ -153,7 +153,7 @@ EOS;
       },
 EOU;
     }
-    
+
     $headers = $headers.<<<EOS
 
       "options"                     : {
@@ -170,20 +170,20 @@ EOU;
           "leaderboard"               : {
             "hide"                    : $hide_leaderboard_page
           }
-        }       
+        }
       },
       "text"                        : {
         "help"                      : "$help_text",
         "help_url"                  : "$help_url"
-      },  
+      },
       "templates"                   : {}
     };
-    
+
     window.onload = function () {
       var root = document.createElement('div');
       root.id = "gimmie-root";
       document.body.appendChild(root);
-      
+
       (function(d){
         var js, id = "gimmie-widget", ref = d.getElementsByTagName("script")[0];
         if (d.getElementById(id)) {return;}
@@ -196,17 +196,17 @@ EOU;
 EOS;
 
   }
-  
+
   if (isset($modSettings['gm_style'])) {
     $stylesheet = $modSettings['gm_style'];
     $headers = $headers.<<<EOS
-    
+
   <style>
 $stylesheet
   </style>
 EOS;
   }
-  
+
   $context['html_headers'] = $headers;
 }
 
@@ -233,10 +233,10 @@ function gimmie_redirect(&$setLocation, &$refresh) {
   );
   list ($board, $owner, $poll_id, $total_replies, $last_message_id, $last_message) = $smcFunc['db_fetch_row']($request);
   $smcFunc['db_free_result']($request);
-  
+
   switch ($context['current_action']) {
     case 'vote':
-      $event = 'did_smf_vote_poll';
+      $event = 'smf_vote_poll';
       if (is_board_enabled($board) && is_event_enabled("gm_trigger_$event")) {
         trigger_event_for_user($event, $email);
       }
@@ -245,67 +245,67 @@ function gimmie_redirect(&$setLocation, &$refresh) {
       $user_action = '';
       $user_id = $user_info['id'];
       if ($total_replies > 0) {
-        $user_action = 'did_smf_reply_thread';
+        $user_action = 'smf_reply_thread';
         if ($user_id == $owner) {
-          $user_action = 'did_smf_reply_own_thread';
+          $user_action = 'smf_reply_own_thread';
         }
       }
       else if ($poll_id) {
-        $user_action = 'did_smf_new_poll';
+        $user_action = 'smf_create_new_poll';
       }
       else {
-        $user_action = 'did_smf_new_thread';
+        $user_action = 'smf_create_new_thread';
       }
-      
+
       if (is_board_enabled($board) && is_event_enabled("gm_trigger_$user_action")) {
         switch ($user_action) {
-          case 'did_smf_new_poll':
+          case 'smf_create_new_poll':
             trigger_event_for_user($user_action, $email);
             break;
-          case 'did_smf_reply_thread':
-          case 'did_smf_reply_own_thread':
-          case 'did_smf_new_thread':
+          case 'smf_reply_thread':
+          case 'smf_reply_own_thread':
+          case 'smf_create_new_thread':
             $keywords = $modSettings['gm_keywords'];
             $keywords = explode(',', $keywords);
             $keywords = array_map('trim', $keywords);
-            
+
             # (not whitespace)<keyword>(not whitespace) or (not whitespace)<another keyword>(not whitespace) or ...
-            $pattern1 = '\W'.implode('\W|\W', $keywords).'\W'; 
+            $pattern1 = '\W'.implode('\W|\W', $keywords).'\W';
             # (start with)<keyword>(not whitespace) or (start with)<another keyword>(not whitespace) or ...
             $pattern2 = '^'.implode('\W|^', $keywords).'\W';
             # (not whitespace)<keyword>(ending) or (not whitespace)<another keyword>(ending) or ...
             $pattern3 = '\W'.implode('$|\W', $keywords).'$';
             $pattern = "/($pattern1|$pattern2|$pattern3)/i";
-            
+
             if (preg_match($pattern, $last_message)) {
               gimmie_log($user_action);
               trigger_event_for_user($user_action, $email);
-            }  
-            
+            }
+
             break;
         }
-        
+
       }
 
       break;
-  }  
+  }
 
-  
+
 }
-  
+
 function gimmie_login_hook($username) {
   global $sourcedir, $context, $modSettings, $user_profile;
   require_once($sourcedir.'/Gimmie.sdk.php');
-  
-  $event = 'did_smf_user_login_time';
+
+  $event = 'smf_login_to_forum';
   if (is_event_enabled("gm_trigger_$event")) {
     $members = loadMemberData(array($username), true);
     $user = $user_profile[array_pop($members)];
-    
+
     $email = $user['email_address'];
     trigger_event_for_user($event, $email);
   }
-  
+
   // SMF Requires this hook returns something.
   return "";
 }
@@ -318,7 +318,7 @@ function gimmie_proxy() {
   require_once($sourcedir.'/Gimmie.sdk.php');
 
   $sa = !empty($_REQUEST['sa']) ? $_REQUEST['sa'] : '';
-  
+
   $id = $context['user']['email'];
 
   $key = $modSettings['gm_key'];
@@ -332,15 +332,15 @@ function gimmie_proxy() {
   $sig_method = new OAuthSignatureMethod_HMAC_SHA1();
   $consumer = new OAuthConsumer($key, $secret, NULL);
   $token = new OAuthConsumer($access_token, $access_token_secret);
-  
+
   $json = '{"response":{"success":false}, "error":{}}';
   if (!is_null($endpoint)) {
     $acc_req = OAuthRequest::from_consumer_and_token($consumer, $token, 'GET', $endpoint, $params);
     $acc_req->sign_request($sig_method, $consumer, $token);
-    
+
     $json = file_get_contents($acc_req);
   }
-  
+
   header('Access-Control-Allow-Origin: *');
   header('Content-type: application/json');
   print($json);
@@ -380,10 +380,10 @@ function gimmie_reward_config_show() {
   isAllowedTo('admin_forum');
   loadLanguage('GimmieRewards');
   loadtemplate('GimmieRewards.admin', array('GimmieRewards.admin'));
-  
+
   require_once($sourcedir.'/Subs-Boards.php');
   getBoardTree();
-  
+
   $context['gimmie_countries'] = json_decode(file_get_contents('https://raw.github.com/mledoze/countries/master/dist/countries.json'));
   $context['gimmie_boards'] = $boards;
 
@@ -396,29 +396,29 @@ function gimmie_reward_config_save() {
 
   isAllowedTo('admin_forum');
   checkSession('post');
-  
+
   $gm_settings = $_REQUEST['gm_settings'];
   $gm_settings['gm_enable'] = (!empty($gm_settings['gm_enable']) ? true : false);
-  
+
   $gm_settings['gm_key'] = (!empty($gm_settings['gm_key']) ? trim($gm_settings['gm_key']) : "e52853bfdcf1d49a0368181245b7");
   $gm_settings['gm_secret'] = (!empty($gm_settings['gm_secret']) ? trim($gm_settings['gm_secret']) : "3a95b4f7da128421ca7a15d67d3b");
   $gm_settings['gm_country'] = (!empty($gm_settings['gm_country']) ? trim($gm_settings['gm_country']) : 'auto');
-  
+
   $gm_settings['gm_notification_timeout'] = (!empty($gm_settings['gm_notification_timeout']) ? intval(trim($gm_settings['gm_notification_timeout'])) : 10);
-  
+
   if (!isset($gm_settings['gm_views_profile'])) {
     $gm_settings['gm_views_profile'] = 0;
   }
-  
+
   if (!isset($gm_settings['gm_views_leaderboard'])) {
     $gm_settings['gm_views_leaderboard'] = 0;
   }
 
   $gm_settings['gm_keywords_forum'] = (!empty($gm_settings['gm_keywords_forum']) ? trim($gm_settings['gm_keywords_forum']) : 'all');
   $gm_settings['gm_keywords'] = (!empty($gm_settings['gm_keywords']) ? trim($gm_settings['gm_keywords']) : "");
-  
+
   gimmie_log($gm_settings);
-  
+
   // Clear context
   $context['gimmie_countries'] = '';
   $context['gimmie_boards'] = '';
@@ -434,7 +434,7 @@ function gimmie_localize_config_show() {
   isAllowedTo('admin_forum');
   loadLanguage('GimmieRewards');
   loadtemplate('GimmieRewards.admin', array('GimmieRewards.admin'));
-  
+
   $context['sub_template'] = 'gimmie_localize_config';
   $context['page_title'] = $txt['gimmie_admin_title'];
 }
@@ -444,16 +444,16 @@ function gimmie_localize_config_save() {
 
   isAllowedTo('admin_forum');
   checkSession('post');
-  
+
   $gm_settings = $_REQUEST['gm_settings'];
-  
+
   $gm_settings['gm_localize'] = (!empty($gm_settings['gm_localize']) ? trim($gm_settings['gm_localize']) : "");
   $gm_settings['gm_localize'] = (json_decode('{'.$gm_settings['gm_localize'].'}') ? $gm_settings['gm_localize'] : '');
-  
+
   $gm_settings['gm_help_text'] = (!empty($gm_settings['gm_help_text']) ? trim($gm_settings['gm_help_text']) : "");
-  $gm_settings['gm_help_url'] = (!empty($gm_settings['gm_help_url']) ? trim($gm_settings['gm_help_url']) : "");  
+  $gm_settings['gm_help_url'] = (!empty($gm_settings['gm_help_url']) ? trim($gm_settings['gm_help_url']) : "");
   gimmie_log($gm_settings);
-  
+
   updateSettings($gm_settings);
   redirectexit('action=admin;area=gmls;sa=settings;gmls_action=saved');
 }
@@ -479,8 +479,8 @@ function is_event_enabled($name) {
 
 function is_board_enabled($board) {
   global $modSettings;
-  return !!(!isset($modSettings['gm_keywords_board']) || 
-            (isset($modSettings['gm_keywords_board']) && 
+  return !!(!isset($modSettings['gm_keywords_board']) ||
+            (isset($modSettings['gm_keywords_board']) &&
             ($modSettings['gm_keywords_board'] == $board || $modSettings['gm_keywords_board'] == 'all')));
 }
 
--- Themes/GimmieRewards.admin.template.php
+++ Themes/GimmieRewards.admin.template.php
@@ -1,19 +1,19 @@
 <?php
-/* 
+/*
  * The MIT License (MIT)
- * 
+ *
  * Copyright (c) 2014 Gimmieworld pte ltd.
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -33,7 +33,7 @@ function template_gimmie_rewards_config() {
 ?>
 
   <div class="admincenter">
-  
+
     <form method="post" action="<?php echo $scripturl; ?>?action=gmss">
       <div class="cat_bar">
         <h3 class="catbg"><?php echo $txt['gimmie_admin_title']; ?></h3>
@@ -66,7 +66,7 @@ function template_gimmie_rewards_config() {
               <input type="text" name="gm_settings[gm_secret]" id="gimmieSecret" size="50" class="input_text gm-input" value="<?php echo (isset($modSettings['gm_secret']) ? htmlspecialchars($modSettings['gm_secret']) : '3a95b4f7da128421ca7a15d67d3b'); ?>">
               <p class="help"><?php echo $txt['gimmie_admin_key_help']; ?></p>
             </dd>
-            
+
             <dt>
               <span><label for="gimmieCountry"><?php echo $txt['gimmie_admin_country_label']; ?></label></span>
             </dt>
@@ -86,24 +86,24 @@ function template_gimmie_rewards_config() {
                 ?>
               </select>
             </dd>
-            
+
             <dt><span><label for="gimmiePopup"><?php echo $txt['gimmie_admin_popup_views']; ?></label></span></dt>
             <dd>
               <input id="gimmieViewProfile" type="checkbox" name="gm_settings[gm_views_profile]" value="1" <?php echo (!isset($modSettings['gm_views_profile']) || (isset ($modSettings['gm_views_profile']) && $modSettings['gm_views_profile']) == 1 ? 'checked' : ''); ?> />
               <label for="gimmieViewProfile"><?php echo $txt['gimmie_admin_popup_profile']; ?></label>
-              
+
               <input id="gimmieViewLeaderboard" type="checkbox" name="gm_settings[gm_views_leaderboard]" value="1" <?php echo (!isset($modSettings['gm_views_leaderboard']) || (isset ($modSettings['gm_views_leaderboard']) && $modSettings['gm_views_leaderboard']) == 1 ? 'checked' : ''); ?> />
               <label for="gimmieViewLeaderboard"><?php echo $txt['gimmie_admin_popup_leaderboard']; ?></label>
             </dd>
-            
+
             <dt><span><label for="gimmieNotificationTimeout"><?php echo $txt['gimmie_admin_notification_timeout']; ?></label></span></dt>
             <dd>
               <input id="gimmieNotificationTimeout" type="number" name="gm_settings[gm_notification_timeout]" value="<?php echo (isset ($modSettings['gm_notification_timeout']) ? intval($modSettings['gm_notification_timeout']) : '10'); ?>" />
             </dd>
           </dl>
-          
+
           <hr class="hrcolor clear">
-          
+
           <dl class="settings">
             <dt><span><label for="gimmieForum"><?php echo $txt['gimmie_admin_keywords_forum']; ?></label></span></dt>
             <dd>
@@ -124,51 +124,51 @@ function template_gimmie_rewards_config() {
                 ?>
               </select>
             </dd>
-          
+
             <dt><span><label for="gimmieKeywords"><?php echo $txt['gimmie_admin_keywords']; ?></label></span></dt>
             <dd>
               <textarea id="gimmieKeywords" name="gm_settings[gm_keywords]" class="gm-input input_textarea" rows="5"><?php echo (isset($modSettings['gm_keywords']) ? $modSettings['gm_keywords'] : ''); ?></textarea>
             </dd>
           </dl>
-          
+
           <hr class="hrcolor clear">
-          
+
           <h1><?php echo $txt['gimmie_admin_trigger_header']; ?></h1>
-          
+
           <dl class="settings">
             <dt><span><label for="gimmieTriggerLogin"><?php echo $txt['gimmie_admin_trigger_login']; ?></label></span></dt>
             <dd>
-              <input id="gimmieTriggerLogin" name="gm_settings[gm_trigger_did_smf_user_login_time]" type="checkbox" value="1" class="input_check" <?php echo (isset ($modSettings['gm_trigger_did_smf_user_login_time']) && $modSettings['gm_trigger_did_smf_user_login_time'] == 1 ? 'checked' : ''); ?> />
+              <input id="gimmieTriggerLogin" name="gm_settings[gm_trigger_smf_login_to_forum]" type="checkbox" value="1" class="input_check" <?php echo (isset ($modSettings['gm_trigger_smf_login_to_forum']) && $modSettings['gm_trigger_smf_login_to_forum'] == 1 ? 'checked' : ''); ?> />
             </dd>
-            
+
             <dt><span><label for="gimmieTriggerNewThread"><?php echo $txt['gimmie_admin_trigger_new_thread']; ?></label></span></dt>
             <dd>
-              <input id="gimmieTriggerNewThread" name="gm_settings[gm_trigger_did_smf_new_thread]" type="checkbox" value="1" class="input_check" <?php echo (isset ($modSettings['gm_trigger_did_smf_new_thread']) && $modSettings['gm_trigger_did_smf_new_thread'] == 1 ? 'checked' : ''); ?> />
+              <input id="gimmieTriggerNewThread" name="gm_settings[gm_trigger_smf_create_new_thread]" type="checkbox" value="1" class="input_check" <?php echo (isset ($modSettings['gm_trigger_smf_create_new_thread']) && $modSettings['gm_trigger_smf_create_new_thread'] == 1 ? 'checked' : ''); ?> />
             </dd>
-            
+
             <dt><span><label for="gimmieTriggerReplyThread"><?php echo $txt['gimmie_admin_trigger_reply']; ?></label></span></dt>
             <dd>
-              <input id="gimmieTriggerReplyThread" name="gm_settings[gm_trigger_did_smf_reply_thread]" type="checkbox" value="1" class="input_check" <?php echo (isset ($modSettings['gm_trigger_did_smf_reply_thread']) && $modSettings['gm_trigger_did_smf_reply_thread'] == 1 ? 'checked' : ''); ?> />
+              <input id="gimmieTriggerReplyThread" name="gm_settings[gm_trigger_smf_reply_thread]" type="checkbox" value="1" class="input_check" <?php echo (isset ($modSettings['gm_trigger_smf_reply_thread']) && $modSettings['gm_trigger_smf_reply_thread'] == 1 ? 'checked' : ''); ?> />
             </dd>
-            
+
             <dt><span><label for="gimmieTriggerReplyOwnThread"><?php echo $txt['gimmie_admin_trigger_reply_own_thread']; ?></label></span></dt>
             <dd>
-              <input id="gimmieTriggerReplyOwnThread" name="gm_settings[gm_trigger_did_smf_reply_own_thread]" type="checkbox" value="1" class="input_check" <?php echo (isset ($modSettings['gm_trigger_did_smf_reply_own_thread']) && $modSettings['gm_trigger_did_smf_reply_own_thread'] == 1 ? 'checked' : ''); ?> />
+              <input id="gimmieTriggerReplyOwnThread" name="gm_settings[gm_trigger_smf_reply_own_thread]" type="checkbox" value="1" class="input_check" <?php echo (isset ($modSettings['gm_trigger_smf_reply_own_thread']) && $modSettings['gm_trigger_smf_reply_own_thread'] == 1 ? 'checked' : ''); ?> />
             </dd>
-            
+
             <dt><span><label for="gimmieTriggerCreatePoll"><?php echo $txt['gimmie_admin_trigger_create_poll']; ?></label></span></dt>
             <dd>
-              <input id="gimmieTriggerCreatePoll" name="gm_settings[gm_trigger_did_smf_new_poll]" type="checkbox" value="1" class="input_check" <?php echo (isset ($modSettings['gm_trigger_did_smf_new_poll']) && $modSettings['gm_trigger_did_smf_new_poll'] == 1 ? 'checked' : ''); ?> />
+              <input id="gimmieTriggerCreatePoll" name="gm_settings[gm_trigger_smf_create_new_poll]" type="checkbox" value="1" class="input_check" <?php echo (isset ($modSettings['gm_trigger_smf_create_new_poll']) && $modSettings['gm_trigger_smf_create_new_poll'] == 1 ? 'checked' : ''); ?> />
             </dd>
-            
+
             <dt><span><label for="gimmieTriggerVotePoll"><?php echo $txt['gimmie_admin_trigger_vote_poll']; ?></label></span></dt>
             <dd>
-              <input id="gimmieTriggerVotePoll" name="gm_settings[gm_trigger_did_smf_vote_poll]" type="checkbox" value="1" class="input_check" <?php echo (isset ($modSettings['gm_trigger_did_smf_vote_poll']) && $modSettings['gm_trigger_did_smf_vote_poll'] == 1 ? 'checked' : ''); ?> />
+              <input id="gimmieTriggerVotePoll" name="gm_settings[gm_trigger_smf_vote_poll]" type="checkbox" value="1" class="input_check" <?php echo (isset ($modSettings['gm_trigger_smf_vote_poll']) && $modSettings['gm_trigger_smf_vote_poll'] == 1 ? 'checked' : ''); ?> />
             </dd>
           </dl>
-          
+
           <hr class="hrcolor clear">
-          
+
           <div class="righttext">
             <input type="submit" value="<?php echo $txt['gimmie_admin_save']; ?>" class="button_submit">
           </div>
@@ -189,7 +189,7 @@ function template_gimmie_localize_config() {
   global $txt, $context, $scripturl, $modSettings;
 ?>
 <div class="admincenter">
-  
+
     <form method="post" action="<?php echo $scripturl; ?>?action=gmls">
       <div class="cat_bar">
         <h3 class="catbg"><?php echo $txt['gimmie_localize_description']; ?></h3>
@@ -198,7 +198,7 @@ function template_gimmie_localize_config() {
       <div class="windowbg2">
         <span class="topslice"></span>
         <div class="content gimmie">
-          
+
           <dl class="settings">
             <dt>
               <span><label for="gimmieStyle"><?php echo $txt['gimmie_style_text']; ?></label></span>
@@ -207,9 +207,9 @@ function template_gimmie_localize_config() {
               <textarea id="gimmieStyle" name="gm_settings[gm_style]" class="gm-input input_textarea" rows="15"><?php echo isset($modSettings['gm_style']) ? $modSettings['gm_style'] : ''; ?></textarea>
             </dd>
           </dl>
-          
+
           <hr class="hrcolor clear">
-                    
+
           <dl class="settings">
             <dt>
               <span><label for="gimmieHelpText"><?php echo $txt['gimmie_help_text']; ?></label></span>
@@ -217,7 +217,7 @@ function template_gimmie_localize_config() {
             <dd>
               <textarea id="gimmieHelpText" name="gm_settings[gm_help_text]" class="gm-input input_textarea" rows="15"><?php echo isset($modSettings['gm_help_text']) ? $modSettings['gm_help_text'] : ''; ?></textarea>
             </dd>
-            
+
             <dt>
               <span><label for="gimmieHelpURL"><?php echo $txt['gimmie_help_url']; ?></label></span>
             </dt>
@@ -225,9 +225,9 @@ function template_gimmie_localize_config() {
               <input id="gimmieHelpURL" type="url" name="gm_settings[gm_help_url]" value="<?php echo (isset($modSettings['gm_help_url']) ? $modSettings['gm_help_url'] : ''); ?>" />
             </dd>
           </dl>
-            
+
           <hr class="hrcolor clear">
-          
+
           <div class="righttext">
             <input type="submit" value="<?php echo $txt['gimmie_admin_save']; ?>" class="button_submit">
           </div>
